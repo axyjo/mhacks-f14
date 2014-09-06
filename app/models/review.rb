cdc627dea23def647827f38a4ec86c6696358914
Akshay Joshi <me@akshayjoshi.com>
2014-09-06 04:13:10 -0400

    Add validation for someone leaving a review for their own publication.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :publication
   belongs_to :user
+
+  validate :author_is_not_reviewer
+
+  def author_is_not_reviewer
+    if self.user == self.publication.user
+      errors.add(:user, 'cannot be the author of the publication')
+    end
+  end
 end
